dc6bee0e81fd2fca6192c25a8ba3a712037ab32a
frank <826389969@qq.com>
2021-10-05 22:49:43 -0400

    stack to solve recursion exceeds

--- homework2/inference_search.py
+++ homework2/inference_search.py
@@ -5,9 +5,10 @@ sys.path.append("..")
 from utils import *
 from homework1.A_star_algo import *
 from shen import *
-
+import gc
 
 # from fang import *
+sys.setrecursionlimit(10 ** 6)
 
 
 class InferenceSearch(object):
@@ -26,73 +27,71 @@ class InferenceSearch(object):
         for i, j in getAllNeighbors(x, self.m, self.n):
             self.B[i][j] += 1
             self.H[i][j] -= 1
-            self.updateCurrent((i, j))
-        # self.B[i][j] += 1
-        # self.H[i][j] -= 1
-        # self.updateCurrent((i, j))
+            self.updateByStack((i, j))
 
     def empty_update(self, x):
         for i, j in getAllNeighbors(x, self.m, self.n):
             self.E[i][j] += 1
             self.H[i][j] -= 1
-            self.updateCurrent((i, j))
+            self.updateByStack((i, j))
 
     def updateMaze(self, x, pivot):
         if pivot == 1:  # C=B
-            nums = countB(x, self.Maze, self.m, self.n, pivot)
-            if nums != self.B[x[0]][x[1]]:
-                print("error", nums, self.B[x[0]][x[1]])
-                return
             for i, j in getAllNeighbors(x, self.m, self.n):
                 if self.Maze[i][j] == 2:
                     self.Maze[i][j] = 0
                     for nei in getAllNeighbors((i, j), self.m, self.n):
                         self.E[nei[0]][nei[1]] += 1
                         self.H[nei[0]][nei[1]] -= 1
-                        self.updateCurrent(nei)
+                        self.updateByStack(nei)
 
         elif pivot == 0:  # N-C=E
-            nums = countB(x, self.Maze, self.m, self.n, pivot)
-            if nums != self.E[x[0]][x[1]]:
-                print("error", nums)
-                return
             for i, j in getAllNeighbors(x, self.m, self.n):
                 if self.Maze[i][j] == 2:
                     self.Maze[i][j] = 1
                     for nei in getAllNeighbors((i, j), self.m, self.n):
                         self.B[nei[0]][nei[1]] += 1
                         self.H[nei[0]][nei[1]] -= 1
-                        self.updateCurrent(nei)
-
-    def updateAllVisited(self, As):
-        # for i, j in self.visited:
-        tmp = set()
-        for t in range(len(self.trajectory)):
-            i, j = self.trajectory[len(self.trajectory) - t - 1]
-            # if (i, j) not in tmp:
-            #     tmp.add((i, j))
-            # else:
-            #     continue
-            self.updateCurrent((i, j))
+                        self.updateByStack(nei)
 
     def updateCurrent(self, x):
         i, j = x
         if self.H[i][j] == 0:
-            # print("H=0", self.N[i][j], self.C[i][j], self.B[i][j], self.E[i][j], self.H[i][j], (i, j))
-            num1 = countB((i, j), self.Maze, self.m, self.n, 0)
-            num2 = countB((i, j), self.Maze, self.m, self.n, 1)
-            # print(num1 + num2, self.N[i][j] - num1 - num2)
             return
         if self.C[i][j] == self.B[i][j]:
-            # print('C=B', self.N[i][j], self.C[i][j], self.B[i][j], self.E[i][j], self.H[i][j], (i, j))
             self.updateMaze((i, j), 1)
-            # print('after C=B', self.N[i][j], self.C[i][j], self.B[i][j], self.E[i][j], self.H[i][j], (i, j))
             return
         if self.N[i][j] - self.C[i][j] == self.E[i][j]:
-            # print('N-C=E', self.N[i][j], self.C[i][j], self.B[i][j], self.E[i][j], self.H[i][j], (i, j))
             self.updateMaze((i, j), 0)
-            # print('after N-C=E', self.N[i][j], self.C[i][j], self.B[i][j], self.E[i][j], self.H[i][j], (i, j))
             return
+
+    def updateByStack(self, x):
+        Enodes, Bnodes = [], []
+        if self.H[x[0]][x[1]] == 0:
+            return
+        node = x
+        sublist = []
+        while len(Enodes) > 0:
+            for i, j in getAllNeighbors(node, self.m, self.n):
+                if self.Maze[i][j] == 2:
+                    self.Maze[i][j] = 0
+                    neilist = getAllNeighbors((i, j), self.m, self.n)
+                    for t in range(len(neilist) - 1, -1, -1):
+                        if self.H[node[0]][node[1]] == 0:
+                            continue
+                        Enodes.append(neilist[t])
+                        if self.C[node[0]][node[1]] == self.B[node[0]][node[1]]:
+                            sublist.append(0)
+                            continue
+                        if self.N[node[0]][node[1]] - self.C[node[0]][node[1]] == self.E[node[0]][node[1]]:
+                            sublist.append(1)
+            node = Enodes.pop()
+            types = sublist.pop()
+            if types == 0:
+                self.E[node[0]][node[1]] += 1
+            else:
+                self.B[node[0]][node[1]] += 1
+            self.H[node[0]][node[1]] -= 1
 
     def inference(self, As):
         # print(As.trajectory)
@@ -122,17 +121,13 @@ class InferenceSearch(object):
     def run(self):
         As = AStar(self.maze, 1)
         while True:
-            # print(As.map.map)
-            # print(self.map.map)
-            # print(self.C)
-            # print(123)
             if not As.run():
                 return False
             block, index = self.inference(As)
             # print(block)
             if index == len(As.trajectory):
-                # print(self.map.map)
-                # print(As.map.map)
+                print(self.map.map)
+                print(As.map.map)
                 return True
             start = As.trajectory[index]
             As.map.start = start
@@ -140,7 +135,7 @@ class InferenceSearch(object):
 
 
 if __name__ == '__main__':
-    p = 0.3
+    p = 0.1
     for i in range(10):
         map = Map(10, 10)
         map.setObstacles(True, p)
@@ -157,3 +152,5 @@ if __name__ == '__main__':
         # print(init[2], map)
         algo = InferenceSearch(map, init)
         print(algo.run())
+        del algo
+        gc.collect()
