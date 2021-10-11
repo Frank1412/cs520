b414707fcd6fa1b0b086002fd27a3373b17e7ad3
cs890 <shenchong328@gmail.com>
2021-10-11 14:24:32 -0400

    staticstics_Shen modified

--- homework2/statistics Shen.py
+++ homework2/statistics Shen.py
@@ -8,7 +8,7 @@ from inference_search import *
 
 if __name__ == "__main__":
     test_num = 1   # 50
-    p_list = np.linspace(0, 0.33, 34)   # 0, 0.33, 34
+    p_list = np.linspace(0, 0.33, 10)   # 0, 0.33, 34
     ATL_list = []   # Average Trajectory Length
     ALT_LSPFDG_list = []  # Length of Trajectory / Length of Shortest Path in Final Discovered Gridworld
     ALSPFDG_LSPFG_list = []   # Length of Shortest Path in Final Discovered Gridworld / Length of Shortest Path in Full Gridworld
@@ -27,6 +27,9 @@ if __name__ == "__main__":
     yourOwn_ANCPR_list = []
     timeFirst = []
     timeSecond = []
+    example_Identified_Cells_list = []
+    yourOwn_Identified_Cells_list = []
+
 
 
 
@@ -36,6 +39,7 @@ if __name__ == "__main__":
         example_ATL, example_ALT_LSPFDG, example_ALSPFDG_LSPFG, example_ANCPR = 0, 0, 0, 0
         yourOwn_ATL,yourOwn_ALT_LSPFDG,yourOwn_ALSPFDG_LSPFG,yourOwn_ANCPR = 0,0,0,0
         timeA, timeB, timeC = 0,0,0
+        example_Identified_Cells,yourOwn_Identified_Cells = 0,0
         for _ in range(test_num):
             r1 = False
             map = Map(101, 101)
@@ -61,7 +65,11 @@ if __name__ == "__main__":
             example_final = AStar(finalMap, 1)
             """"""
             example_final.run()
+
+            example_Identified_Cells += sum(sum(example_al.Maze != 2))
+
             example_ALT_LSPFDG += (len(example_al.trajectory) * 1.0 / len(example_final.trajectory))
+            print(len(example_final.trajectory))
             example_ALSPFDG_LSPFG += (len(example_final.trajectory) * 1.0 / len(As.trajectory))
             timeA += time2 - time1
 
@@ -73,6 +81,7 @@ if __name__ == "__main__":
             own_inference.run(trick=True)
             time4 = time.time()
             timeB += time4-time3
+            # print(timeB)
             # Length of Shortest Path in Final Discovered Gridworld
 
             # yourOwn_final =  AStar(own_inference.Maze,1)
@@ -81,7 +90,10 @@ if __name__ == "__main__":
             yourOwn_final = AStar(finalMap1,1)
 
             yourOwn_final.run()
-            yourOwn_ALT_LSPFDG+=(len(own_inference.trajectory)*1.0/len(yourOwn_final.trajectory))
+
+            yourOwn_Identified_Cells += sum(sum(own_inference.Maze!=2))
+            # print(yourOwn_Identified_Cells)
+            yourOwn_ALT_LSPFDG += (len(own_inference.trajectory)*1.0/len(yourOwn_final.trajectory))
             yourOwn_ALSPFDG_LSPFG += (len(yourOwn_final.trajectory)*1.0/len(As.trajectory))
 
 
@@ -96,14 +108,25 @@ if __name__ == "__main__":
 
 
 
+
+
+
         # example
         example_ATL_list.append(example_ATL / test_num)
+        yourOwn_ATL_list.append(yourOwn_ATL/test_num)
         timeFirst.append(timeA / test_num)
         timeSecond.append(timeB / test_num)
+        example_ALT_LSPFDG_list.append(example_ALT_LSPFDG/test_num)
+        example_ALSPFDG_LSPFG_list.append(example_ALSPFDG_LSPFG/test_num)
+        example_ANCPR_list.append(example_ATL / test_num)
+        yourOwn_ALT_LSPFDG_list.append(yourOwn_ALT_LSPFDG/test_num)
+        yourOwn_ALSPFDG_LSPFG_list.append(yourOwn_ALSPFDG_LSPFG/test_num)
+        yourOwn_ANCPR_list.append(yourOwn_ATL/test_num)
 
 
 
 
+   #Density vs Average Trajectory Length
     plt.plot(p_list, example_ATL_list, color="green")
     plt.plot(p_list, yourOwn_ATL_list, color="red")
     plt.legend(["example inference","Own_Inference"])
@@ -111,11 +134,44 @@ if __name__ == "__main__":
     plt.ylabel("Average Trajectory Length")
     plt.show()
 
+#Density vs Average (Length of Trajectory / Length of Shortest Path in Final Discovered Gridworld)
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    ax.plot(p_list, timeFirst, 'green', label="original")
-    ax.plot(p_list, timeSecond, 'red', label="bump into the cell")
+    ax.plot(p_list, example_ALT_LSPFDG_list, 'green', label="example inference")
+    ax.plot(p_list, yourOwn_ALT_LSPFDG_list, 'red', label="Own_Inference")
+    plt.xlabel("density")
+    plt.ylabel("Length of Trajectory / Length of Shortest Path in Final Discovered Gridworld")
+    plt.legend()
+    plt.show()
 
+    #Density vs Average (Length of Shortest Path in Final Discovered Gridworld / Length of Shortest
+    #Path in Full Gridworld)
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(p_list, example_ALSPFDG_LSPFG_list, 'green', label="example inference")
+    ax.plot(p_list, yourOwn_ALSPFDG_LSPFG_list, 'red', label="Own_Inference")
+    plt.xlabel("density")
+    plt.ylabel("Length of Shortest Path in Final Discovered Gridworld / Length of Shortest Path in Full Gridworld")
+    plt.legend()
+    plt.show()
+
+    #Density vs Average Number of Cells Processed by Repeated A*
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(p_list,example_ANCPR_list, 'green', label="example inference")
+    ax.plot(p_list, yourOwn_ANCPR_list, 'red', label="Own_Inference")
+    plt.xlabel("density")
+    plt.ylabel("Density vs Average Number of Cells Processed by Repeated A*")
+    plt.legend()
+    plt.show()
+
+    #Runtime
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(p_list, timeFirst, 'green', label="example inference")
+    ax.plot(p_list, timeSecond, 'red', label="Own_Inference")
     plt.xlabel("density")
     plt.ylabel("Runtime")
     plt.legend()
